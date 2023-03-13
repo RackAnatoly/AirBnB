aada4dae84d55e350a3073f90f068646223a4396
Anatoli Rak <Rack.anatoly@gmail.com>
2023-03-13 16:41:13 +0100

    first commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+import Entype from 'react-native-vector-icons/Entypo';
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView></SafeAreaView>
+    </>
+  );
+}
+
+export default App;
